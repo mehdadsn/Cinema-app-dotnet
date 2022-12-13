0c114af390166481aeddf0766ede0359a79eb622
mehdadsn <mehrdadsn@outlook.com>
2022-12-14 00:50:48 +0330

    adding identity

---
+++ Cinema/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace CinemaApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string? Phone { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
---
+++ Cinema/ViewModel/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CinemaApp.ViewModel
+{
+    public class LoginViewModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
